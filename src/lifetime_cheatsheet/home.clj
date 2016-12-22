f9e5c6e3a6fd85cd218f9eb1c7eeda09673573aa
jeaye <contact@jeaye.com>
2016-12-21 18:01:47 -0800

    Add JS-based highlighting

--- src/lifetime_cheatsheet/home.clj
+++ src/lifetime_cheatsheet/home.clj
@@ -7,16 +7,26 @@
 
 (def read-example (comp slurp io/resource :file))
 
+(defn code [code-str]
+  [:pre
+   [:code {:class "c"}
+    code-str]])
+
 (defn row [example]
   (let [bad-str (-> example :bad read-example)
         good-str (-> example :good read-example)]
     [:tr
-     [:td bad-str]
-     [:td good-str]]))
+     [:td (code bad-str)]
+     [:td (code good-str)]]))
 
 (defn html []
   (page/html5
-    [:head [:style (css/main)]]
+    [:head
+     [:style (css/main)]
+     [:link {:rel "stylesheet"
+              :href "//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/solarized-light.min.css"}]
+     [:script {:src "//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/highlight.min.js"}]
+     [:script "hljs.initHighlightingOnLoad();"]]
     [:body
      [:table.grid
       (for [ex (:examples @config/data)]
