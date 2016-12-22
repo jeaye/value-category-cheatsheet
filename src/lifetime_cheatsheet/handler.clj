d6d28e364c87b046c2e458e28640db8685b75963
jeaye <contact@jeaye.com>
2016-12-21 16:38:24 -0800

    Add inital compojure project

---
+++ src/lifetime_cheatsheet/handler.clj
@@ -0,0 +1,11 @@
+(ns lifetime-cheatsheet.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
